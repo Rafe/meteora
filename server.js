2a45a3688a413988db97f5cd1f63bff503cdc590
Jimmy Chao <daizenga@gmail.com>
2015-06-12 19:08:32 -0400

    Add initial prototype

--- server.js
+++ server.js
@@ -1,31 +1,25 @@
 var React = require('react')
-var App = React.createFactory(require('./App'))
-var DOM = React.DOM, body = DOM.body, div = DOM.div, script = DOM.script;
+var Article = React.createFactory(require('./components/article'))
+var DOM = React.DOM, body = DOM.body, div = DOM.div, script = DOM.script
 
-var props = {
-      items: [
-        'Item 0',
-        'Item 1',
-        'Item </script>',
-        'Item <!--inject!-->',
-      ]
-    }
+var resources = [
+  'this is article 1',
+  'this is article 2',
+  'this is article 3',
+  'this is article 4'
+]
+
 var html = React.renderToStaticMarkup(body(null,
-  // The actual server-side rendering of our component occurs here, and we
-  // pass our data in as `props`. This div is the same one that the client
-  // will "render" into on the browser from browser.js
   div({id: 'content', dangerouslySetInnerHTML: {__html:
-    React.renderToString(App(props))
+    React.renderToString(div(null, resources.map(function(resource) {
+      return Article({content: resource})
+    })))
   }}),
-
-  // We'll load React from a CDN - you don't have to do this,
-  // you can bundle it up or serve it locally if you like
-  script({src: '//fb.me/react-0.13.3.min.js'}),
-
-  // Then the browser will fetch and run the browserified bundle consisting
-  // of browser.js and all its dependencies.
-  // We serve this from the endpoint a few lines down.
-  script({src: '/bundle.js'})
+  script({src: 'https://cdnjs.cloudflare.com/ajax/libs/react/0.13.3/react.js'})
 ))
 
-console.log(html)
+var fs = require('fs')
+
+fs.writeFile('./test.html', html, function(err) {
+  console.log('file is saved!')
+});
